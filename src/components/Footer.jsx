18d1877acda9aa7027e0819d5663ffe60d630e7c
Dan-Swarts <Danstraws@gmail.com>
2024-10-30 18:48:47 -0500

    restored tailwind config path to link the background image with the '/' character

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,5 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
-import '../styles/background.css';
 
 export default function Footer() {
     return (
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ export default {
   theme: {
     extend: {
       backgroundImage: {
+        'header': "url('/assets/images/background1.png')", 
       },
     },
   },
