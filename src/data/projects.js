4d8aad9c04a30d3084822e8e32680a69faf42194
Dan-Swarts <Danstraws@gmail.com>
2024-10-30 19:50:22 -0500

    added 3 more projects to the portfolio

--- src/data/projects.js
+++ src/data/projects.js
@@ -18,6 +18,24 @@ const projects = [
         subtitle: 'Create your own chess board',
         link: 'https://github.com/RainesArt/AlarmBoard',
     },
+    {
+        image: "assets/images/employeeTracker.png",
+        title: 'Employee Tracker',
+        subtitle: 'Manage your employees',
+        link: 'https://github.com/Dan-Swarts/Employee-Tracker',
+    },
+    {
+        image: "assets/images/rockPaperScissors.png",
+        title: 'Rock Paper Scissors',
+        subtitle: 'Play against a random generator!',
+        link: 'https://github.com/Dan-Swarts/rock-paper-scissors',
+    },
+    {
+        image: "assets/images/vehivleManagement.png",
+        title: 'Vehicle Management System',
+        subtitle: 'Track all kinds of information!',
+        link: 'https://github.com/Dan-Swarts/Vehicle-Management-System',
+    },
 ];
 
 export default projects;
